1593cef5608eba8fda08952a9d87b80ad81197ab
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-14 14:45:06 +0200

    Revert "Revert "added time and space complexity""
    This reverts commit 706f3df9c9f73706db27850def1c0bb723b67417.

---
+++ ClockAngleCalculatorTests/ClockAngleCalculatorMainProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using ClockAngleCalculatorNS;
+
+namespace ClockAngleCalculatorTests
+{
+    [TestClass]
+    public class ClockAngleCalculatorMainProgramTests
+    {
+
+        public void NumberOfHoursReceivedShouldBeLessThanTwelve() { }
+
+        public void NumberOfMinutesReceivedShouldBeLessThanSixty() { }
+
+        public void ShouldTakeInIntegerValueForHoursValue() { }
+
+        public void ShouldTakeInIntegerValueForMinutesValue() { }
+    }
+
+
+}
