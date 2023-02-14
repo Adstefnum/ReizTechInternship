eb8ed979e199e2235c55d08a1e3fabb24479af65
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-14 04:55:48 +0200

    main program

--- clockAngle/Program.cs
+++ clockAngle/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+
+namespace ClockAngle
+{
+    class Program
+       
+    {
+        private static int hours, minutes;
+        static void Main(string[] args)
+        {
+            ReceiveHoursAndMinutesFromUser();
+            ClockAngleCalculator clockAngleCalculator = new ClockAngleCalculator(hours, minutes);
+            double angle = clockAngleCalculator.CalculateSmallerClockAngle();
+            PrintClockAngle(angle);
+        }
+
+        static void ReceiveHoursAndMinutesFromUser()
+        {
+            Console.WriteLine("Enter the hours:");
+            hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the minutes:");
+            minutes = Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void PrintClockAngle(double angle)
+        {
+            Console.WriteLine("The angle between the long hand and short hand is: " + angle);
+        }
+
+
+    }
+}
