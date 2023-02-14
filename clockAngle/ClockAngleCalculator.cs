e206fb27b9689e7477fc6430e5cdcc2e26a2f860
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-14 04:55:40 +0200

    class containing clock angle calculation logic

---
+++ clockAngle/ClockAngleCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClockAngle
+{
+    public class ClockAngleCalculator
+    {
+        private int hours, minutes;
+        public ClockAngleCalculator(int _hours,int _minutes)
+        {
+            hours = _hours;
+            minutes = _minutes;
+
+        }
+
+       public double CalculateSmallerClockAngle()
+        {
+            double hourAngle = 0.5 * (60 * hours + minutes);
+            double minuteAngle = 6 * minutes;
+            double angle = Math.Abs(hourAngle - minuteAngle);
+            angle = Math.Min(360 - angle, angle);
+            return angle;
+        }
+    }
+}
