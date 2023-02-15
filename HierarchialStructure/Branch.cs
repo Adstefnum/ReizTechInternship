03db4755e81d8f1ffe1f539b7822c1db01b43270
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-15 02:31:12 +0200

    starting of the branches project

---
+++ HierarchialStructure/Branch.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace HierarchialStructure
+{
+    public class Branch
+    {
+        List<Branch> branches;
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+
+        public void AddBranch() { }
+
+        public void DetermineDepth() { }
+
+    }
+}
