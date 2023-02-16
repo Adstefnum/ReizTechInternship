35a0690b9ad41ec6bedc71c90df71acbf0c8f611
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-16 18:44:19 +0200

    the branch program main

--- HierarchialStructure/Program.cs
+++ HierarchialStructure/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace HierarchialStructureNS
+{
+    public class Program {
+
+        static void Main(string[] args)
+        {
+            Branch root = new Branch();
+            root.AddBranch(2);
+            root.branches[0].AddBranch(1);
+            root.branches[1].AddBranch(3);
+
+            root.branches[1].branches[0].AddBranch(1);
+            root.branches[1].branches[1].AddBranch(2);
+
+            root.branches[1].branches[1].branches[0].AddBranch(1);
+
+
+            Console.Write("The depth of the structure is " + root.DetermineDepth());
+
+        }
+
+        
+
+    } 
+}
